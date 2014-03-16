488c1051b0a8563cd4aac84bf425b2cec1ac0bf0
Joseph Maruschek <joe.maruschek@gmail.com>
2014-03-16 14:38:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my version of the Ep007 program.  It uses code from Ep006.
---
+++ SpeedProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEp007
+{
+    class SpeedProvider
+    {
+        /// <summary>
+        /// Convert Miles Per Hour to Kilometers Per Hour
+        /// </summary>
+        /// <param name="speedInMPH">Miles per hour as a whole number</param>
+        /// <returns>Returns Kilometers per hour as a whole number</returns>
+        public static int ConvertMPHtoKPH(int speedInMPH)
+        {
+            return (int)Math.Round(speedInMPH * 1.609344, 0);
+        }
+
+        /// <summary>
+        /// Calculates Speed based on distance in miles and minutes until your appointment
+        /// </summary>
+        /// <param name="distanceInMiles">Distance as a decimale</param>
+        /// <param name="minutesLeft">Minutes as a decimal</param>
+        /// <returns>Speed in MPH as a whole number</returns>
+        public static int RequiredSpeedInMPH(decimal distanceInMiles, decimal minutesLeft)
+        {
+            int requiredSpeed = 0; //initialize a variable for the calculated speed to return/
+
+            decimal amountOfHours = minutesLeft / 60; // 60 mins in an hour
+            requiredSpeed = (int)Math.Round(distanceInMiles / amountOfHours, 0); //Use the Math library to round the decimal to the nearest whole number
+
+            return requiredSpeed; //return the speed back to the function caller.
+        }
+    }
+}
